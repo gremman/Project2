143177067009fe78470c7a63b2b9928faa6b0c30
Joshua Moore <jommoore2003@me.com>
2019-01-14 20:35:47 -0800

    Testing Travis

--- Trash_Pandas/models/index.js
+++ Trash_Pandas/models/index.js
@@ -10,7 +10,8 @@ var db = {};
 
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
-} else {
+} 
+      else {
   var sequelize = new Sequelize(
     config.database,
     config.username,
